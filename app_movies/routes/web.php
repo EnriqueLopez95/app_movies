8740922f40fce40054a5c7291d84d92edd0c7901
Enrique Lopez <enrqlpz15@gmail.com>
2023-10-30 00:15:24 -0600

    Added AdminLTN to the project and the view and the administrator route are created

--- app_movies/routes/web.php
+++ app_movies/routes/web.php
@@ -27,3 +27,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 // Route::get('/index', function () {
 //     return view('index');
 // });
+
+//RUTAS PARA EL ADMINISTRADOR
+Route::get('/admin', function () {
+    return view('admin.index');
+});
